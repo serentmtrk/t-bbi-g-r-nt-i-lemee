2869efb05bb432dac536ab0f3ba417766f4a6cde
serentmtrk <45974007+serentmtrk@users.noreply.github.com>
2018-12-18 19:54:52 +0300

    Add files via upload

---
+++ Yeni klasör/arduino_kodu/arduino_kodu.ino
@@ -0,0 +1,131 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(1,OUTPUT);
+
+}
+
+void loop(){
+
+  
+char c = Serial.read();
+
+if (c=='1')
+{
+        digitalWrite(1,HIGH);
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,LOW);
+      
+}
+     else  if (c=='2')
+{
+       digitalWrite(2,HIGH);
+        digitalWrite(1,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,LOW);
+}
+  else  if (c=='3')
+{
+       digitalWrite(3,HIGH);
+        digitalWrite(2,LOW);
+        digitalWrite(1,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,LOW);
+}
+  else  if (c=='4')
+{
+        digitalWrite(4,HIGH);
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(1,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,LOW);
+}
+  else  if (c=='5')
+{
+        digitalWrite(5,HIGH);
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(1,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,LOW);
+}
+  else  if (c=='6')
+{
+        digitalWrite(6,HIGH);
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(1,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,LOW);
+}
+  else  if (c=='7')
+{
+      digitalWrite(7,HIGH);
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(1,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,LOW);
+}
+  else  if (c=='8')
+{
+       digitalWrite(8,HIGH);
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(1,LOW);
+        digitalWrite(9,LOW);
+}
+  else  if (c=='9')
+{
+       digitalWrite(9,HIGH);
+        digitalWrite(2,LOW);
+        digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(1,LOW);
+}
+}
